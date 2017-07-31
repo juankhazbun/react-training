a7bf4bb2c0d9bed9314514ead06fe26e6476ad62
Juank Hazbun <juan.hazbun@koombea.com>
2017-07-31 08:55:41 -0500

    Create sections dynamically from an array

--- src/components/SectionList.js
+++ src/components/SectionList.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import Section from './Section';
 
+var exps = [
+  {
+    'title': 'Experiencia 1',
+    'message': 'Esta es mi experiencia #1'
+  },
+  {
+    'title': 'Experiencia 2',
+    'message': 'Esta es mi experiencia #2'
+  },
+  {
+    'title': 'Experiencia 3',
+    'message': 'Esta es mi experiencia #3'
+  },
+];
+
 class SectionList extends Component {
+
   render() {
+    var sections = exps.map(function(exp) {
+                     return (
+                        <Section
+                          title={exp.title}
+                          message={exp.message}
+                        />
+                     )
+                 });
     return (
       <div>
-        <Section
-          key="1"
-          title="Experiencia"
-          message="Esta es mi experiancia"
-        />
-        <Section
-          key="2"
-          title="Experiencia2"
-          message="Esta es mi experiancia2"
-        />
-        <Section
-          key="3"
-          title="Experiencia3"
-          message="Esta es mi experiancia3"
-        />
+        { sections }
       </div>
     );
   }
